06c670d2be94c07f00a1c66ec21d6493f2775aec
brandon <bberning.web@gmail.com>
2021-05-05 10:45:11 -0400

    created an info comp that displays information on every page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Work from './Pages/Work/Work'
 import Contact from './Pages/Contact/Contact'
 import About from './Pages/About/About'
+import Info from './Comps/Info';
 import Nav from './Nav'
 import './Styles/App.css';
 
@@ -17,6 +18,7 @@ const App = () => {
         <Route path="/about" exact component={About} />
         <Route path="/contact" exact component={Contact} />
       </Switch>
+      <Info />
     </Router>
   )}
 

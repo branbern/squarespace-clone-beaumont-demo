20ba9ea3d029aad93a04910f8ad320d97a61f1e8
brandon <bberning.web@gmail.com>
2021-05-06 08:45:36 -0400

    moved nav to componenets

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
-
-
-import Work from './Pages/Work/Work'
-import Contact from './Pages/Contact/Contact'
-import About from './Pages/About/About'
+import Work from './Pages/Work/Work';
+import Contact from './Pages/Contact/Contact';
+import About from './Pages/About/About';
 import Info from './Comps/Info';
-import Nav from './Nav'
+import Nav from './Comps/Nav';
 import './Styles/App.css';
 
 const App = () => {
--- src/Comps/Nav.jsx
+++ src/Comps/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useLocation } from "react-router-dom";
-import './Styles/Nav.scss';
+import '../Styles/Nav.scss';
 
 const Nav = () => {
 
